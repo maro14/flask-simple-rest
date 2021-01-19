17f3a74934cda8cae9766e229a86852b5c1d2ea6
eki <eki1490@gmail.com>
2021-01-19 15:39:54 +0700

    add route

---
+++ api/db/route.py
@@ -0,0 +1,8 @@
+
+ROUTE = [
+     {"lat": 64.0027441, "long": -22.7066262, "name": "Pulomas", "status": "kuning"},
+     {"lat": 64.0317168, "long": -22.1092311, "name": "Asmi", "status": "Merah"},
+     {"lat": 64.0317168, "long": -22.1092311, "name": "Pendongkelan", "status": "Merah"},
+     {"lat": 64.0317168, "long": -22.1092311, "name": "Cempaka Mas", "status": "Merah"}
+]
+
