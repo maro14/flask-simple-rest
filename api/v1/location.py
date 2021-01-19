f79d5df3515519cee2fcf1d8b34c86756874600b
eki <eki1490@gmail.com>
2021-01-19 21:05:16 +0700

    simple route

--- api/v1/location.py
+++ api/v1/location.py
@@ -3,7 +3,15 @@ from api.db.route import ROUTE
 
 location = Blueprint('v1', __name__)
 
-@location.route('/location')
+@location.route('/location', methods=["GET"])
 def get_location():
     return jsonify({'location': ROUTE})
 
+
+@location.route('/location/<name_route>')
+def get_name(name_route):
+    name_found = [
+        name for name in ROUTE if name['name'] == name_route]
+    if (len(name_found) > 0):
+        return jsonify({'name': name_found[0]})
+    return jsonify({'massage': 'Name not found'})
--- api/v2/coordinate.py
+++ api/v2/coordinate.py
@@ -1,8 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from api.db.route import ROUTE
 
 coordinate = Blueprint('v2', __name__)
 
 
 @coordinate.route('/coordinate')
 def get_cordinate():
-    return {'Hello': 'Coordinate'}
+    return jsonify(ROUTE)
+
+@coordinate.route('/coordinate/<lat>')
+def get_lag(lat):
+    the_coordinate = [
+        lat for lat in ROUTE if lat['lat'] == lat
+    ]
+    return {'latitude': the_coordinate[0]}
